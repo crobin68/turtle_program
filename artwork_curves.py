3259ec2fd46f3ff4f8865931998c91fba8ff84f0
crobin68 <crobin68@msudenver.edu>
2024-04-03 14:00:49 -0600

    Create artwork_curves.py

---
+++ artwork_curves.py
@@ -0,0 +1,14 @@
+import turtle
+
+# Set up the screen
+wn = turtle.Screen()
+wn.setup(width=600, height=600)
+wn.bgcolor("black")
+
+artwork = turtle.Turtle()
+artwork.color("white")
+artwork.pendown()
+
+
+
+turtle.exitonclick()
