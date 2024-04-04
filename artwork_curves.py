933ecbac088749ef282afd6763da7a477b89ebe1
crobin68 <crobin68@msudenver.edu>
2024-04-03 20:58:41 -0600

    thing

--- artwork_curves.py
+++ artwork_curves.py
@@ -1,4 +1,5 @@
 import turtle
+import random
 
 # Set up the screen
 wn = turtle.Screen()
@@ -8,6 +9,40 @@ wn.bgcolor("black")
 artwork = turtle.Turtle()
 artwork.color("white")
 artwork.pendown()
+
+
+# make turtle random colors
+def random_color():
+    r = random.random()
+    g = random.random()
+    b = random.random()
+    return (r, g, b)
+
+
+
+change = 90
+
+artwork.speed(0)
+for __ in range(20):
+    artwork.color(random_color())
+    for _ in range(70):
+        
+        artwork.forward(10)
+        artwork.right(5)
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 turtle.exitonclick()
