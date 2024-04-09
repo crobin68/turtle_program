9c62f01be9a82beb975f98403f1589da83180137
crobin68 <crobin68@msudenver.edu>
2024-04-09 09:46:50 -0600

    created new file called 123.py

---
+++ new_project.py/123.py
@@ -0,0 +1,22 @@
+import turtle
+
+
+wn = turtle.Screen()
+wn.bgcolor("black")
+
+
+# Creation of turtles
+alex = turtle.Turtle()
+bob = turtle.Turtle()
+
+# set color for turtles 
+alex.pencolor("white")
+bob.pencolor("Red")
+
+
+
+
+
+
+
+wn.exitonclick()
